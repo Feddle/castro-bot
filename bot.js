920295a23af07700e704736eaa15244b3da2620b
Eppu Peltonen <eppu.peltonen@icloud.com>
2018-02-09 18:55:48 +0200

    commands now working

--- bot.js
+++ bot.js
@@ -6,21 +6,20 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (message.substring(0,1) == '!')
-  {
-      var args = message.substring(1).split(' ');
-      var cmd = args[0];
+client.on('message', message => {
+    if (message.content.substring(0,1) == '!') {
+        var args = message.content.substring(1).split(' ');
+        var cmd = args[0];
+        args = args.splice(1)
 
-      args = args.splice(1);
-
-      switch(cmd)
-      {
-          case 'ketaootetaan' :
-            msg.reply('Mikkoo venattu joku 4h.');
-            break;
-      }
-  }
-});
+        // commandit
+        switch(cmd)
+        {
+            case 'ketaootetaan' :
+                message.channel.send('Roopea odotettu joku 4h kohta.');
+                break;
+        }
+    }
+  });
 
 client.login(auth.token);
