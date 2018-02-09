ec92f93600dd4f5cbba264f871df24f8e15c071a
Eppu Peltonen <eppu.peltonen@icloud.com>
2018-02-09 21:59:24 +0200

    command stuff

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const { prefix, token } = require('./config.json');
 
-var commands = [];
+var commands = ["ketaootetaan","miika","cyanide", "lmao"];
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -56,19 +56,20 @@ client.on('message', message =>
     }
     else if (message.content.startsWith(`${prefix}ketaootetaan`)) 
     {
-        commands.push('ketaootetaan');
         message.channel.send('Roopea odotettu joku 4h kohta.');
     }
     else if (message.content.startsWith(`${prefix}miika`)) 
     {
-        commands.push('miika');
         message.channel.send('Miika on homo.');
     }
     else if (message.content.startsWith(`${prefix}cyanide`)) 
     {
-        commands.push('cyanide');
         var text = getStreamer();
         message.channel.send(text);
+    }
+    else if (message.content.startsWith(`${prefix}lmao`)) 
+    {
+        message.channel.send('LMFAO!');
     }
 });
 
