d32d05e62d54d9ec05126f041fc2c5882c7800b5
Feddle <sepptee@outlook.com>
2018-02-10 03:58:52 +0200

    paranneltiin twitch toimintoa tilanteessa jos käyttäjä on offline tai käyttäjää ei löydy

--- bot.js
+++ bot.js
@@ -7,43 +7,62 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-
-function getStreamer(twitchUser, url = "https://wind-bow.glitch.me/twitch-api/streams/" + twitchUser ) {
-
-    return fetch(url)
-      .then(handleTwitchResponse)      
-      .catch(error => console.log(error));     
-      
+//Hakee annetun striimaajan
+function getStreamer(twitchUser) {
+    var streams = "https://wind-bow.glitch.me/twitch-api/streams/" + twitchUser;    
+    return fetch(streams)
+    .then(
+    function(response) {                    
+            var textResponse;
+            return response.json()
+                .then(json => {
+                  if (response.ok && json["stream"] != null) {                                                        
+                        var link = json["stream"]["channel"]["url"];
+                        var game = json["stream"]["channel"]["game"];
+                        var status = json["stream"]["channel"]["status"];
+                        var displayName = json["stream"]["channel"]["display_name"];
+                        var logo = json["stream"]["channel"]["logo"];    
+                        textResponse = displayName + " is streaming " + game + "\n" + status + "\n" + link;            
+                        return textResponse;
+                    } else if(json["stream"] == null) {                            
+                            var name = json["_links"]["self"].split("/");
+                            return getChannel(name[name.length - 1]);
+                    } else {
+                        return Promise.reject(Object.assign({}, json, {
+                        status: response.status,
+                        statusText: response.statusText
+                        }))
+                    }
+                })
+    }
+    );           
 }
 
-function handleTwitchResponse(response) {
-    var textResponse;
-    return response.json()
-        .then(json => {
-          if (response.ok) {
-            if(json["stream"] == null) {                
-                var link = json["url"];            
-                var displayName = json["display_name"];
-                var logo = json["logo"];                      
-                textResponse = displayName + " is offline";
-                return textResponse;      
-            } else {
-                var link = json["stream"]["channel"]["url"];
-                var game = json["stream"]["channel"]["game"];
-                var status = json["stream"]["channel"]["status"];
-                var displayName = json["stream"]["channel"]["display_name"];
-                var logo = json["stream"]["channel"]["logo"];    
-                textResponse = displayName + " is streaming " + game + "\n" + status + "\n" + link;            
-                return textResponse;
-            }
-          } else {
-            return Promise.reject(Object.assign({}, json, {
-              status: response.status,
-              statusText: response.statusText
-            }))
-          }
-        })
+//Hakee annetun twitch kanavan
+function getChannel(twitchUser) {
+    var channels = "https://wind-bow.glitch.me/twitch-api/channels/" + twitchUser;
+    return fetch(channels)
+    .then(
+    function(response) {                    
+            var textResponse;
+            return response.json()
+                .then(json => {
+                  if (response.ok && json["display_name"]) {                                                        
+                        var link = json["url"];                        
+                        var displayName = json["display_name"];                                                 
+                        textResponse = displayName + " is offline" + "\n" + link;
+                        return textResponse;
+                    } else {
+                        return Promise.reject(Object.assign({}, json, {
+                        status: response.status,
+                        statusText: response.statusText
+                        }))
+                    }
+                })
+    }
+    );   
 }
+
 
 
 
@@ -76,7 +95,9 @@ client.on('message', message =>
         var sentence = message.content.split(' ');
         var twitchUser = sentence[1];
 
-        getStreamer(twitchUser).then(response => message.channel.send(response));        
+        getStreamer(twitchUser)
+        .then(response => message.channel.send(response))
+        .catch(error => {console.log(error); message.channel.send(error["message"])})       
     }
 });
 
