1c961d43d047c8904932888c4a4c266145d1ab3b
Feddle <sepptee@outlook.com>
2018-02-27 20:13:51 +0200

    ketaootetaan leaderboard

--- commands/ketaootetaan.js
+++ commands/ketaootetaan.js
@@ -1,5 +1,7 @@
 
-var waitingList = {};    
+const fs = require('fs');
+var waitingList = {};  
+var leaderboard; 
 
 function add(id) {    
     waitingList[id].time.seconds++;
@@ -23,7 +25,8 @@ function timer(id) {
 }
 
 function start(id) {
-    waitingList[id] = {timer: undefined, time: {seconds: 0, minutes: 0, hours: 0}, clock: ""};
+    waitingList[id] = {timer: undefined, time: {seconds: 0, minutes: 0, hours: 0}, clock: ""}; 
+    if(leaderboard[id] == undefined) leaderboard[id] = "00:00:00";    
     timer(id);
 }
 
@@ -36,6 +39,19 @@ function clear(id) {
     delete waitingList[id];
 }
 
+function sortLeaderboard() {
+    var arr = [];
+    for(var id in leaderboard) {        
+        arr.push({[id]:leaderboard[id]});
+    }
+    arr.sort(function(a,b) {
+        var timeA = parseInt(Object.values(a)[0].replace(":", "")); 
+        var timeB = parseInt(Object.values(b)[0].replace(":", ""));         
+        return timeB - timeA;
+    });
+    return arr;
+}
+
 
 
 module.exports = {
@@ -43,36 +59,74 @@ module.exports = {
     description: 'KETÄ OOTETAAN?',
 
     execute(message, args, client) 
-    {    	         
-        var user = message.mentions.members.first();
-        var id = user.user.id;
-        console.log(waitingList);
-        if(user.voiceChannel !== undefined && message.member.voiceChannel.id == user.voiceChannel.id) {
-            message.channel.send(user + " on jo kanavalla");
-            return;
-        }
-        if(waitingList[id]) {
-            message.channel.send("henkilöä " + user + " ootetaan jo");
-            return;
-        }
-        start(id);
-        var listener;        
+    {    	    
+        fs.readFile('./logs/leaderboard_KO.json', (err, data) => {
+           if(err) throw err; 
+           leaderboard = JSON.parse(data);
+           
+            if(args == "leaderboard") {
+                var arr = sortLeaderboard();                
+                var embed = {
+                  "title": "**Ketaootetaan Leaderboard**",
+                  "fields": []
+                };
+                for(let i = 0; i < arr.length; i++) {
+                    let id = Object.keys(arr[i])[0];                    
+                    embed["fields"].push({"name": "#"+(i+1), "value": client.users.get(id).username + " - " + Object.values(arr[i])[0]});
+                }
+                message.channel.send({ embed });                               
+                return;
+            }
+            
+            var user = message.mentions.members.first();
+            var id = user.user.id;        
+            if(user.voiceChannel !== undefined && message.member.voiceChannel.id == user.voiceChannel.id) {
+                message.channel.send(user + " on jo kanavalla");
+                return;
+            }
+            if(waitingList[id]) {
+                message.channel.send("henkilöä " + user + " ootetaan jo");
+                return;
+            }
+            start(id);                
+            var interval = setInterval(function() {
+                leaderboard[id] = waitingList[id].clock;
+                
+                fs.writeFile('./logs/leaderboard_KO.json', JSON.stringify(leaderboard), (err) => {  
+                    if (err) throw err;                    
+                });
+            },60000);            
+            var listener;        
+            
+            client.on('voiceStateUpdate', listener = (oldMember, newMember) => 
+            {
+              let newUserChannel = newMember.voiceChannel
+              let oldUserChannel = oldMember.voiceChannel
+
+
+              if(oldUserChannel === undefined && newUserChannel !== undefined) {
+                  if(newUserChannel.members.get(id)) {
+                      stop(id);
+                      message.channel.send("henkilöä "+ user + " ootettiin joku " + waitingList[id].clock);
+                      clearInterval(interval);
+                      var timeBefore = leaderboard[id].split(":");                      
+                      var seconds = parseInt(timeBefore[2]) + waitingList[id].time.seconds;
+                      var minutes = parseInt(timeBefore[1]) + waitingList[id].time.minutes;
+                      var hours = parseInt(timeBefore[0]) + waitingList[id].time.hours;
+                      var timeAfter = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + 
+                                    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + 
+                                    (seconds > 9 ? seconds : "0" + seconds);
+                      leaderboard[id] = timeAfter;
+                      fs.writeFile('./logs/leaderboard_KO.json', JSON.stringify(leaderboard), (err) => {  
+                            if (err) throw err;                            
+                        });
+                      clear(id);
+                      client.removeListener("voiceStateUpdate", listener);
+                  }
+                   
+              } 
+            })
+        });
         
-		client.on('voiceStateUpdate', listener = (oldMember, newMember) => 
-        {
-          let newUserChannel = newMember.voiceChannel
-          let oldUserChannel = oldMember.voiceChannel
-
-
-          if(oldUserChannel === undefined && newUserChannel !== undefined) {
-              if(newUserChannel.members.get(id)) {
-                  stop(id);
-                  message.channel.send("henkilöä "+ user + " ootettiin joku " + waitingList[id].clock);
-                  clear(id);
-                  client.removeListener("voiceStateUpdate", listener);
-              }
-               
-          } 
-        })
     },
 };
