7bcf0fac2f900635dbb73e8354b12219eb9970ec
Feddle <sepptee@outlook.com>
2019-03-13 11:58:48 +0200

    refactor: changed corgi to koiru

---
+++ commands/koiru.js
@@ -0,0 +1,25 @@
+const logger = require("../logger.js");
+
+module.exports = {
+  name: "koiru",
+  description: "posts a random koiru picture from dogehouse channel",
+  usage: "",
+  execute(message, args, client) {        
+    let koiru_ch = client.channels.get("427803623892844544");        
+
+    koiru_ch.fetchMessages()
+      .then(messages => {        
+        let koiru_attach;
+        while(!koiru_attach) {
+          let koiru_msg = messages.random();
+          try {
+            koiru_attach = koiru_msg.attachments.first().url;
+          } catch(e) {logger.warn(e);}
+        }
+        return message.channel.send("", {files: [koiru_attach]});
+      })
+      .catch(err => {logger.error(err); return "Ei löytynyt koiru viestiä";});
+  },
+};
+
+
