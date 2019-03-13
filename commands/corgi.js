842cecdae9c7f910286a70239185b80d32451ec9
Feddle <sepptee@outlook.com>
2019-03-13 10:43:11 +0200

    fix: handling for messages without url

--- commands/corgi.js
+++ commands/corgi.js
@@ -5,12 +5,17 @@ module.exports = {
   description: "posts a random corgi picture from corgihouse channel",
   usage: "",
   execute(message, args, client) {        
-    var corgi_ch = client.channels.get("427803623892844544");        
+    let corgi_ch = client.channels.get("427803623892844544");        
 
     corgi_ch.fetchMessages()
       .then(messages => {
-        var corgi_msg = messages.random();
-        var corgi_attach = corgi_msg.attachments.first().url;
+        let corgi_msg = messages.random();
+        let corgi_attach;
+        while(!corgi_attach) {
+          try {
+            corgi_attach = corgi_msg.attachments.first().url;
+          } catch(e) {logger.warn(e);}
+        }
         return message.channel.send("", {files: [corgi_attach]});
       })
       .catch(err => {logger.error(err); return "Ei löytynyt corgi viestiä";});
