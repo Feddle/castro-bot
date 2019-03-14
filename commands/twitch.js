3802c1566b6701d173fbeff2531e6d82909fde31
Feddle <sepptee@outlook.com>
2019-03-14 12:22:48 +0200

    fix: fixed config path

--- commands/twitch.js
+++ commands/twitch.js
@@ -1,5 +1,5 @@
 const fetch = require("node-fetch");
-const {twitchClientID} = require("../config.json");
+const {twitchClientID} = require(__dirname +"/../config.json");
 const logger = require("../logger.js");
 
 //Hakee annetun striimaajan
