275fbc7f256a5a62c6bc74afcbc3be56e51a3b4c
Feddle <sepptee@outlook.com>
2019-03-15 12:01:05 +0200

    feat: created main body for print command

---
+++ commands/print.js
@@ -0,0 +1,84 @@
+
+//ei taida onnistua koska user, message jne objetkeja ei saa muutettua stringiksi jotta voisi lähettää pastebiniin
+
+const logger = require("../logger.js");
+const axios = require("axios");
+const util = require("util");
+const {pastebinKey} = require(__dirname + "/../config.json");
+
+module.exports = {
+  name: "print",
+  description: "Posts a pastebin link containing the discord.js object (message/user/guild/channel) of the given type for the given id.",
+  usage: "!print [type] [id]",
+  execute(message, args, client) {  
+    const types = ["message", "user", "guild", "channel"];
+    if(!types.includes(args[0])) {message.channel.send("ei sitä komentoa noin käytetä"); return;}  
+
+    let ch = message.channel.id;    
+    logger.debug(args);
+
+    getSnowflake(args, client, ch)
+      .then(snowflake => getPastebin(snowflake))
+      .then(link => {      
+        if(link) message.channel.send(link);
+        else logger.error("Pastebin link was empty");  
+      });                          
+  },
+};
+
+async function getSnowflake(args, client, ch) {
+  let obj;
+  switch(args[0]) {
+  case "message": obj = getMessage(args[1], client, ch); break;
+  case "user": obj = getUser(args[1], client, ch) ;break;
+  case "guild": obj = getGuild(args[1], client, ch); break;
+  case "channel": obj = getChannel(args[1], client, ch); break;  
+  }
+  return await obj;
+}
+
+async function getPastebin(snowflake) {
+  const api = "https://pastebin.com/api/api_post.php";    
+  let snowflakeString = util.inspect(snowflake);
+  //logger.debug(snowflakeString);
+  let parameters = {
+    api_dev_key: pastebinKey,
+    api_option: "paste",
+    api_paste_code: "snowflakeString",
+    api_paste_format: "json",
+    api_paste_private: 1,
+    api_paste_expire_date: "1D",
+    headers: {"content-type": "application/x-www-form-urlencoded"}
+  };
+  let pastebin = axios.post(api, JSON.stringify(parameters))
+    .then(response => response)
+    .then(text => {pastebin = text; logger.debug(pastebin);});
+
+  return await pastebin;
+}
+
+async function getMessage(id, client, ch) {
+  let res = client.channels.get(ch).fetchMessage(id)
+    .then(message => res = message)
+    .catch(err => {logger.error(err); return "Ei löytynyt viestiä";});
+  return await res;
+}
+
+function getUser(id, client) {
+  let user = client.users.get(id);  
+  if(!user) return "Ei löytynyt käyttäjää";
+  return user;
+}
+
+function getGuild(id, client) {
+  let guild = client.guilds.get(id);  
+  if(!guild) return "Ei löytynyt kiltaa";
+  return guild;
+}
+
+function getChannel(id, client) {
+  let channel = client.channels.get(id);  
+  if(!channel) return "Ei löytynyt kanavaa";
+  return channel;
+}
+
