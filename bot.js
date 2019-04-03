0075003ba2b379cf2d110267ee0a950c209538b8
Feddle <sepptee@outlook.com>
2019-04-03 14:59:27 +0300

    fix: added discordjs handler for error event

--- bot.js
+++ bot.js
@@ -1,10 +1,7 @@
 const fs = require("fs");
 const Discord = require("discord.js");
-const {
-  prefix,
-  token
-} = require(__dirname +"/config.json");
-const logger = require(__dirname +"/logger.js");
+const { prefix, token } = require(__dirname + "/config.json");
+const logger = require(__dirname + "/logger.js");
 
 var t0 = process.hrtime();
 
@@ -13,7 +10,7 @@ var t0 = process.hrtime();
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync(__dirname +"/commands");
+const commandFiles = fs.readdirSync(__dirname + "/commands");
 
 for (const file of commandFiles) {
   // require the command file
@@ -24,32 +21,42 @@ for (const file of commandFiles) {
   client.commands.set(command.name, command);
 }
 
+client.on("error", e => {
+  logger.error(e);
+});
+
 //Check if there was a crash, try to print the stack and rename the file
-client.on("ready", () => {	
-  var timeInMilliseconds = process.hrtime(t0)[1]/1000000; // dividing by 1000000 gives milliseconds from nanoseconds
+client.on("ready", () => {
+  var timeInMilliseconds = process.hrtime(t0)[1] / 1000000; // dividing by 1000000 gives milliseconds from nanoseconds
   logger.info("Startup took " + timeInMilliseconds + " milliseconds.");
   logger.info("Logged in as " + client.user.tag);
 
-  fs.readFile(__dirname +"/logs/crash.log", (err, data) => {
-    if(err) logger.warn("Could not read crash.log: " + err);
+  fs.readFile(__dirname + "/logs/crash.log", (err, data) => {
+    if (err) logger.warn("Could not read crash.log: " + err);
     else {
       try {
         var d = JSON.parse(data).stack;
-      } catch(err) {
+      } catch (err) {
         logger.error("Could not parse crash.log: " + err);
         logger.info("Removing crash.log\n" + data);
-        fs.unlink(__dirname +"/logs/crash.log", (err) => {
-          if(err) logger.error("Could not remove crash.log:" + err);
+        fs.unlink(__dirname + "/logs/crash.log", err => {
+          if (err) logger.error("Could not remove crash.log:" + err);
           else logger.info("Removed crash.log");
           d = "Error parsing crash log";
         });
       }
-      if(!d) d = "something went wrong :(";
-      client.channels.get("423839512666439692").send("Kaadoit botin, tässä pino: \n" + "```\n" + d + "```");
+      if (!d) d = "something went wrong :(";
+      client.channels
+        .get("423839512666439692")
+        .send("Kaadoit botin, tässä pino: \n" + "```\n" + d + "```");
       let date = new Date().toISOString().replace(/:/g, "-");
-      fs.rename(__dirname +"/logs/crash.log", __dirname +"/logs/" + date + "-crash.log", function(err) {
-        if (err) logger.error("Error renaming crash.log: " + err);			
-      });
+      fs.rename(
+        __dirname + "/logs/crash.log",
+        __dirname + "/logs/" + date + "-crash.log",
+        function(err) {
+          if (err) logger.error("Error renaming crash.log: " + err);
+        }
+      );
     }
   });
 });
@@ -58,11 +65,11 @@ var vitunRegex = /(vitun[\s]bot)/;
 var goodRegex = /(good[\s]bot)/;
 
 client.on("message", message => {
-  if(message.channel.id == "427803623892844544") {
-    if(message.attachments.size <= 0) message.delete();	
-    //giveCorgiMessageId -> DiscordAPIError: Cannot edit a message authored by another user				
+  if (message.channel.id == "427803623892844544") {
+    if (message.attachments.size <= 0) message.delete();
+    //giveCorgiMessageId -> DiscordAPIError: Cannot edit a message authored by another user
     return;
-  }		
+  }
   if (message.content.toLowerCase().match(goodRegex)) {
     replyHappy(message);
     return;
@@ -71,23 +78,26 @@ client.on("message", message => {
     replySad(message);
     return;
   }
-  if (!message.content.startsWith(prefix) || message.author.bot)
-    return;			
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content.slice(prefix.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
 
-  const command = client.commands.get(commandName)
-	|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+  const command =
+    client.commands.get(commandName) ||
+    client.commands.find(
+      cmd => cmd.aliases && cmd.aliases.includes(commandName)
+    );
 
-  if (!command)
-    return;
+  if (!command) return;
 
   if (command.args && !args.length) {
     let reply = "You didn't provide any arguments!";
 
     if (command.usage) {
-      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${
+        command.usage
+      }\``;
     }
     return message.channel.send(reply);
   }
@@ -100,10 +110,9 @@ client.on("message", message => {
   }
 });
 
-
 client.login(token);
 
-function replyHappy(message) {	
+function replyHappy(message) {
   var castroE = client.emojis.find("name", "castro").toString();
   var arr = [":)", ":3", ";^)", castroE];
   message.channel.send(arr[getRandomInt(arr.length)]);
@@ -112,29 +121,39 @@ function replyHappy(message) {
 function replySad(message) {
   var castroE = client.emojis.find("name", "castro").toString();
   var mullekoalatE = client.emojis.find("name", "mullekoalat").toString();
-  var arr = [":(", mullekoalatE, ":3", "haluutko turpaas", "No ite vittu koodasit :D homo :Dd", castroE];	
+  var arr = [
+    ":(",
+    mullekoalatE,
+    ":3",
+    "haluutko turpaas",
+    "No ite vittu koodasit :D homo :Dd",
+    castroE
+  ];
   var reply = arr[getRandomInt(arr.length)];
   message.channel.send(reply);
-  setTimeout(replyTurpaas, 500, message, reply);	
+  setTimeout(replyTurpaas, 500, message, reply);
 }
 
 function replyTurpaas(message, reply) {
-  if(reply == "haluutko turpaas") {		
+  if (reply == "haluutko turpaas") {
     var listener;
-    client.on("message", listener = function(msg) {			
-      switch (msg.content) {
-      case "en":				
-        message.channel.send("saat silti");
-        client.removeListener("message", listener);
-        break;			
-      case "homo":
-        message.channel.send("ite oot");
-        client.removeListener("message", listener);
-        break;
-      default:
-        client.removeListener("message", listener);
-      }
-    });
+    client.on(
+      "message",
+      (listener = function(msg) {
+        switch (msg.content) {
+          case "en":
+            message.channel.send("saat silti");
+            client.removeListener("message", listener);
+            break;
+          case "homo":
+            message.channel.send("ite oot");
+            client.removeListener("message", listener);
+            break;
+          default:
+            client.removeListener("message", listener);
+        }
+      })
+    );
   }
 }
 
