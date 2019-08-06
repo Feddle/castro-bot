867a87681c2a3cef18577b7f8514e82e21365e6d
Feddle <sepptee@outlook.com>
2019-08-06 15:58:24 +0300

    style: changes to code styling according to airbnb linter

--- commands/twitch.js
+++ commands/twitch.js
@@ -1,22 +1,24 @@
-const fetch = require("node-fetch");
-const {twitchClientID} = require(__dirname +"/../config.json");
-const logger = require("../logger.js");
+const fetch = require('node-fetch');
 
-//Fetch the givne streamer
+const configPath = `${__dirname}/../config.json`;
+const { twitchClientID } = require(configPath);
+const logger = require('../logger.js');
+
+// Fetch the given streamer
 function getStreamer(twitchUser) {
-  let streams = "https://api.twitch.tv/kraken/streams/" + twitchUser;
-  let myInit = {
-    method: "GET",
+  const streams = `https://api.twitch.tv/kraken/streams/'${twitchUser}`;
+  const myInit = {
+    method: 'GET',
     headers: {
-      "Client-ID": twitchClientID
-    }
+      'Client-ID': twitchClientID,
+    },
   };
   return fetch(streams, myInit)
     .then(
-      function (response) {
-        let embed;		
+      (response) => {
+        let embed;
         return response.json()
-          .then(json => {
+          .then((json) => {
             if (response.ok && json["stream"] != null) {
               logger.info("Fetched twitch stream: " + twitchUser);
               let link = json["stream"]["channel"]["url"];
@@ -40,7 +42,7 @@ function getStreamer(twitchUser) {
               };
 
               return embed;
-            } else if (json["stream"] == null && json["_links"] != null) {
+            } if (json["stream"] == null && json["_links"] != null) {
               let name = json["_links"]["self"].split("/");
               return getChannel(name[name.length - 1]);
             } else {
@@ -50,68 +52,68 @@ function getStreamer(twitchUser) {
               }));
             }
           });
-      });
+      },
+    );
 }
 
-//Fetch the given channel
+// Fetch the given channel
 function getChannel(twitchUser) {
-  let channels = "https://api.twitch.tv/kraken/channels/" + twitchUser;
-  let myInit = {
-    method: "GET",
+  const channels = `https://api.twitch.tv/kraken/channels/${twitchUser}`;
+  const myInit = {
+    method: 'GET',
     headers: {
-      "Client-ID": twitchClientID
-    }
+      'Client-ID': twitchClientID,
+    },
   };
   return fetch(channels, myInit)
     .then(
-      function (response) {
-        let textResponse;
+      (response) => {
         return response.json()
-          .then(json => {
-            if (response.ok && json["display_name"]) {
-              logger.info("Fetched twitch channel: " + twitchUser);
-              let link = json["url"];
-              let displayName = json["display_name"];				
-              let logo = json["logo"];
-              let embed = {
-                "description": displayName + " is offline",
-                "url": link,
-                "color": 7032241,					  
-                "thumbnail": {
-                  "url": logo
+          .then((json) => {
+            if (response.ok && json.display_name) {
+              logger.info('Fetched twitch channel: ' + twitchUser);
+              const link = json.url;
+              const displayName = json.display_name;
+              const logo = json.logo;
+              const embed = {
+                'description': `${displayName  } is offline`,
+                'url': link,
+                'color': 7032241,
+                'thumbnail': {
+                  'url': logo,
                 },
-                "author": {
-                  "name": displayName,
-                  "url": link					
-                }
+                author: {
+                  name: displayName,
+                  url: link,
+                },
               };
               return embed;
             } else {
               return Promise.reject(Object.assign({}, json, {
                 status: response.status,
-                statusText: response.statusText
+                statusText: response.statusText,
               }));
             }
           });
-      });
+      },
+    );
 }
 
 module.exports = {
-  name: "twitch",
-  description: "fetches twitch channel link and states whether the streamer is online or not",
+  name: 'twitch',
+  description: 'fetches twitch channel link and states whether the streamer is online or not',
   args: true,
-  usage: "[twitchUser]",
+  usage: '[twitchUser]',
   execute(message, args) {
-
     // Extract twitch user from command
-    let sentence = message.content.split(" ");
-    let twitchUser = sentence[1];
+    const sentence = message.content.split(' ');
+    const twitchUser = sentence[1];
 
     getStreamer(twitchUser)
-      .then(embed => {return message.channel.send({embed});})
-      .catch(error => {
+      .then(embed => message.channel.send({ embed }))
+      .catch((error) => {
         logger.error(error);
-        message.channel.send(error["message"]);
+        message.channel.send(error.message);
       });
   },
 };
