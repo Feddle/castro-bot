e2f39d8a1edd6cef5b4cc17d0fe511d10bd42b77
Feddle <sepptee@outlook.com>
2019-08-07 13:42:50 +0300

    feat: refactored crash reporting and added error handlers and checks

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 
-const { prefix, token } = require(`${__dirname }/config.json`);
+const { prefix, token } = require(`${__dirname}/config.json`);
 const logger = require(`${__dirname}/logger.js`);
 
 const t0 = process.hrtime();
@@ -25,41 +25,133 @@ client.on('error', (e) => {
   logger.error(e);
 });
 
-// Check if there was a crash, try to print the stack and rename the file
+/**
+ * Removes crash.log
+ * @return {Object} {err:} if error, else {}
+ */
+function removeCrashLog() {
+  logger.info('Removing crash.log');
+  fs.unlink(`${__dirname}/logs/crash.log`, (err) => {
+    if (err) {
+      const errorMsg = `Could not remove crash.log: ${err}`;
+      logger.error(errorMsg);
+      return { err: errorMsg };
+    }
+    logger.info('Removed crash.log');
+    return {};
+  });
+}
+
+/**
+ * Parses data to JSON and returns crash stack if succesful otherwise returns error
+ * @todo Test to see if data object has something more that could be used
+ * @param {string} data - data to parse
+ * @return {Object} {err:} if error, else {stack:}
+ */
+function parseCrashLog(data) {
+  let crashStack;
+  let errorMsg;
+  try {
+    crashStack = JSON.parse(data).stack;
+  } catch (err) {
+    errorMsg = `Could not parse crash.log: ${err}`;
+    logger.error(errorMsg);
+    logger.debug(`crash.log: ${data}`);
+    removeCrashLog();
+
+    return { err: errorMsg };
+  }
+  return { stack: crashStack };
+}
+
+/**
+ * Reads crash.log and returns it's contents
+ * Returns error if unsuccesful
+ * @return {Object} {err:} if error, else {data:}
+ */
+function readCrashLog() {
+  fs.readFile(`${__dirname}/logs/crash.log`, (readErr, data) => {
+    if (readErr) {
+      const errorMsg = `Could not read crash.log: ${readErr}`;
+      logger.warn(errorMsg);
+      return { err: errorMsg };
+    }
+    return { data };
+  });
+}
+
+/**
+ * Renames crash.log to [date]-crash.log
+ * @return {Object} {err:} if error, else {}
+ */
+function renameCrashLog() {
+  const date = new Date().toISOString().replace(/:/g, '-');
+  fs.rename(
+    `${__dirname}/logs/crash.log`,
+    `${__dirname}/logs/${date}-crash.log`,
+    (err) => {
+      if (err) {
+        const errorMsg = `Error renaming crash.log: ${err}`;
+        logger.error(errorMsg);
+        return { err: errorMsg };
+      }
+      return {};
+    },
+  );
+}
+
+/**
+ * Handles the previous crash by creating [date]-crash.log and sending the stack to given channel
+ * @todo Test this
+ * @param {string} - id of the channel to report the stack to
+ */
+async function handlePreviousCrash(channel) {
+  /* Read crash.log */
+  const readResult = await readCrashLog();
+  if (readResult.err) {
+    logger.error(readResult.err);
+    removeCrashLog();
+    client.channels
+      .get(channel)
+      .send(`crash.log was found but could not be read: ${readResult.err}`);
+    return;
+  }
+
+  /* Parse crash.log */
+  const parseResult = parseCrashLog();
+  if (parseResult.err) {
+    logger.error(parseResult.err);
+    removeCrashLog();
+    client.channels
+      .get(channel)
+      .send(`crash.log was found but could not be parsed: ${parseResult.err}`);
+    return;
+  }
+
+  /* Report stack */
+  client.channels
+    .get(channel)
+    .send(`Kaadoit botin, tässä pino: \n\`\`\`\n${parseResult.stack}\`\`\``);
+
+  /* Rename crash.log */
+  const renameResult = renameCrashLog();
+  if (renameResult.err) {
+    logger.error(renameResult.err);
+    removeCrashLog();
+    client.channels
+      .get(channel)
+      .send(`crash.log was found but could not be renamed: ${renameResult.err}`);
+  }
+}
+
 client.on('ready', () => {
   // dividing by 1000000 gives milliseconds from nanoseconds
   const timeInMilliseconds = process.hrtime(t0)[1] / 1000000;
   logger.info(`Startup took ${timeInMilliseconds} milliseconds.`);
   logger.info(`Logged in as ${client.user.tag}`);
 
-  fs.readFile(`${__dirname}/logs/crash.log`, (err, data) => {
-    if (err) logger.warn(`Could not read crash.log: ${err}`);
-    else {
-      try {
-        var d = JSON.parse(data).stack;
-      } catch (err) {
-        logger.error(`Could not parse crash.log: ${err}`);
-        logger.info(`Removing crash.log\n${data}`);
-        fs.unlink(`${__dirname}/logs/crash.log`, (err) => {
-          if (err) logger.error(`Could not remove crash.log:${err}`);
-          else logger.info('Removed crash.log');
-          d = 'Error parsing crash log';
-        });
-      }
-      if (!d) d = 'something went wrong :(';
-      client.channels
-        .get('423839512666439692')
-        .send(`Kaadoit botin, tässä pino: \n\`\`\`\n${d}\`\`\``);
-      const date = new Date().toISOString().replace(/:/g, '-');
-      fs.rename(
-        `${__dirname}/logs/crash.log`,
-        `${__dirname}/logs/${date}-crash.log`,
-        (err) => {
-          if (err) logger.error(`Error renaming crash.log: ${err}`);
-        },
-      );
-    }
-  });
+  const crashReportChannel = '423839512666439692';
+  if (fs.existsSync(`${__dirname}/logs/crash.log`)) handlePreviousCrash(crashReportChannel);
 });
 
 const vitunRegex = /(vitun[\s]bot)/;
