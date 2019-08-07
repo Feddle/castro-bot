a42639e34ddc1155feb1d9e4deab6359a64bfbdd
Feddle <sepptee@outlook.com>
2019-08-07 10:42:01 +0300

    style: changes to code styling

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
-const fs = require("fs");
-const Discord = require("discord.js");
-const { prefix, token } = require(__dirname + "/config.json");
-const logger = require(__dirname + "/logger.js");
+const fs = require('fs');
+const Discord = require('discord.js');
 
-var t0 = process.hrtime();
+const { prefix, token } = require(`${__dirname }/config.json`);
+const logger = require(`${__dirname}/logger.js`);
 
-//------command functionality--------
+const t0 = process.hrtime();
+
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync(__dirname + "/commands");
+const commandFiles = fs.readdirSync(`${__dirname}/commands`);
 
 for (const file of commandFiles) {
   // require the command file
@@ -21,55 +21,51 @@ for (const file of commandFiles) {
   client.commands.set(command.name, command);
 }
 
-client.on("error", e => {
+client.on('error', (e) => {
   logger.error(e);
 });
 
-//Check if there was a crash, try to print the stack and rename the file
-client.on("ready", () => {
-  var timeInMilliseconds = process.hrtime(t0)[1] / 1000000; // dividing by 1000000 gives milliseconds from nanoseconds
-  logger.info("Startup took " + timeInMilliseconds + " milliseconds.");
-  logger.info("Logged in as " + client.user.tag);
+// Check if there was a crash, try to print the stack and rename the file
+client.on('ready', () => {
+  // dividing by 1000000 gives milliseconds from nanoseconds
+  const timeInMilliseconds = process.hrtime(t0)[1] / 1000000;
+  logger.info(`Startup took ${timeInMilliseconds} milliseconds.`);
+  logger.info(`Logged in as ${client.user.tag}`);
 
-  fs.readFile(__dirname + "/logs/crash.log", (err, data) => {
-    if (err) logger.warn("Could not read crash.log: " + err);
+  fs.readFile(`${__dirname}/logs/crash.log`, (err, data) => {
+    if (err) logger.warn(`Could not read crash.log: ${err}`);
     else {
       try {
         var d = JSON.parse(data).stack;
       } catch (err) {
-        logger.error("Could not parse crash.log: " + err);
-        logger.info("Removing crash.log\n" + data);
-        fs.unlink(__dirname + "/logs/crash.log", err => {
-          if (err) logger.error("Could not remove crash.log:" + err);
-          else logger.info("Removed crash.log");
-          d = "Error parsing crash log";
+        logger.error(`Could not parse crash.log: ${err}`);
+        logger.info(`Removing crash.log\n${data}`);
+        fs.unlink(`${__dirname}/logs/crash.log`, (err) => {
+          if (err) logger.error(`Could not remove crash.log:${err}`);
+          else logger.info('Removed crash.log');
+          d = 'Error parsing crash log';
         });
       }
-      if (!d) d = "something went wrong :(";
+      if (!d) d = 'something went wrong :(';
       client.channels
-        .get("423839512666439692")
-        .send("Kaadoit botin, tässä pino: \n" + "```\n" + d + "```");
-      let date = new Date().toISOString().replace(/:/g, "-");
+        .get('423839512666439692')
+        .send(`Kaadoit botin, tässä pino: \n\`\`\`\n${d}\`\`\``);
+      const date = new Date().toISOString().replace(/:/g, '-');
       fs.rename(
-        __dirname + "/logs/crash.log",
-        __dirname + "/logs/" + date + "-crash.log",
-        function(err) {
-          if (err) logger.error("Error renaming crash.log: " + err);
-        }
+        `${__dirname}/logs/crash.log`,
+        `${__dirname}/logs/${date}-crash.log`,
+        (err) => {
+          if (err) logger.error(`Error renaming crash.log: ${err}`);
+        },
       );
     }
   });
 });
 
-var vitunRegex = /(vitun[\s]bot)/;
-var goodRegex = /(good[\s]bot)/;
+const vitunRegex = /(vitun[\s]bot)/;
+const goodRegex = /(good[\s]bot)/;
 
-client.on("message", message => {
-  if (message.channel.id == "427803623892844544") {
-    if (message.attachments.size <= 0) message.delete();
-    //giveCorgiMessageId -> DiscordAPIError: Cannot edit a message authored by another user
-    return;
-  }
+client.on('message', (message) => {
   if (message.content.toLowerCase().match(goodRegex)) {
     replyHappy(message);
     return;
@@ -83,10 +79,9 @@ client.on("message", message => {
   const args = message.content.slice(prefix.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
 
-  const command =
-    client.commands.get(commandName) ||
-    client.commands.find(
-      cmd => cmd.aliases && cmd.aliases.includes(commandName)
+  const command = client.commands.get(commandName)
+    || client.commands.find(
+      cmd => cmd.aliases && cmd.aliases.includes(commandName),
     );
 
   if (!command) return;
@@ -95,82 +90,68 @@ client.on("message", message => {
     let reply = "You didn't provide any arguments!";
 
     if (command.usage) {
-      reply += `\nThe proper usage would be: \`${prefix}${command.name} ${
-        command.usage
-      }\``;
+      reply += `\nThe proper usage would be: 
+        \`${prefix}${command.name} ${command.usage}\``;
     }
-    return message.channel.send(reply);
+    message.channel.send(reply);
   }
 
   try {
     command.execute(message, args, client);
   } catch (error) {
-    logger.info("Could not execute a command: " + error);
-    message.reply("fix your shit noob");
+    logger.info(`Could not execute a command: ${error}`);
+    message.reply('fix your shit noob');
   }
 });
 
 client.login(token);
 
 function replyHappy(message) {
-  var castroE = client.emojis.find("name", "castro").toString();
-  var arr = [":)", ":3", ";^)", castroE];
+  const castroE = client.emojis.find('name', 'castro').toString();
+  const arr = [':)', ':3', ';^)', castroE];
   message.channel.send(arr[getRandomInt(arr.length)]);
 }
 
 function replySad(message) {
-  var castroE = client.emojis.find("name", "castro").toString();
-  var mullekoalatE = client.emojis.find("name", "mullekoalat").toString();
-  var arr = [
-    ":(",
+  const castroE = client.emojis.find('name', 'castro').toString();
+  const mullekoalatE = client.emojis.find('name', 'mullekoalat').toString();
+  const arr = [
+    ':(',
     mullekoalatE,
-    ":3",
-    "haluutko turpaas",
-    "No ite vittu koodasit :D homo :Dd",
-    castroE
+    ':3',
+    'haluutko turpaas',
+    'No ite vittu koodasit :D homo :Dd',
+    castroE,
   ];
-  var reply = arr[getRandomInt(arr.length)];
+  const reply = arr[getRandomInt(arr.length)];
   message.channel.send(reply);
   setTimeout(replyTurpaas, 500, message, reply);
 }
 
+/* Reply for if user responded to replySad */
+/* TODO: This is not the way to make a chat bot */
 function replyTurpaas(message, reply) {
-  if (reply == "haluutko turpaas") {
-    var listener;
-    client.on(
-      "message",
-      (listener = function(msg) {
-        switch (msg.content) {
-          case "en":
-            message.channel.send("saat silti");
-            client.removeListener("message", listener);
-            break;
-          case "homo":
-            message.channel.send("ite oot");
-            client.removeListener("message", listener);
-            break;
-          default:
-            client.removeListener("message", listener);
-        }
-      })
-    );
+  if (reply === 'haluutko turpaas') {
+    const listener = (msg) => {
+      switch (msg.content) {
+        case 'en':
+          message.channel.send('saat silti');
+          client.removeListener('message', listener);
+          break;
+        case 'homo':
+          message.channel.send('ite oot');
+          client.removeListener('message', listener);
+          break;
+        default:
+          client.removeListener('message', listener);
+      }
+    };
+    client.on('message', listener);
   }
 }
 
-/*function giveCorgiMessageId() {
-	var corgi_ch = client.channels.get("427803623892844544");        
-        corgi_ch.fetchMessages({ limit: 2 })		
-		.then(messages => {			
-			let message_content = messages.last().content.split(" ");
-			let id = Number.parseInt(message_content[message_content.length - 1]) + 1;			
-			let new_content = message.content == "" ? id : " " + id;
-			message.edit(new_content)
-			.then(msg => logger.info(`Edited corgi message: ${msg}`))
-			.catch(err => {logger.error(err); return "Ongelma muokatessa corgi viestiä";});			
-		})
-		.catch(err => {logger.error(err); return "Ongelma muokatessa corgi viestiä";});	
-}*/
-
+/* Returns random positive integer */
+/* TODO: if more fucntions like these are needed move this to utils.js */
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
