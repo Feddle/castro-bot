0eb9e7cd4427b18d47ee8e9597c2f55395809a7d
Feddle <sepptee@outlook.com>
2019-08-14 14:28:56 +0300

    style: ran eslint

--- commands/miika.js
+++ commands/miika.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: "miika",
-  description: "States truths about Miika",
-  usage: "",
-  execute(message, args) {
-    message.channel.send("Meekah is a faggot", {
-      tts: true
+  name: 'miika',
+  description: 'States truths about Miika',
+  usage: '',
+  execute(message) {
+    message.channel.send('Meekah is a faggot', {
+      tts: true,
     });
   },
 };
