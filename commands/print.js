09895dc559ff789b5419a391f79c822833f80711
Feddle <sepptee@outlook.com>
2019-08-16 11:08:04 +0300

    feat: added better error handling and some docs

--- commands/print.js
+++ commands/print.js
@@ -2,39 +2,10 @@
 const axios = require('axios');
 const util = require('util');
 
-const { pastebinKey } = require(`${__dirname }/../config.json`);
 const querystring = require('querystring');
+const { pastebinKey } = require('../config.json');
 const logger = require('../logger.js');
 
-module.exports = {
-  name: 'print',
-  description: 'Posts a pastebin link containing the discord.js object (message/user/guild/channel) of the given type for the given id.',
-  usage: '[type] [id]',
-  execute(message, args, client) {
-    const types = ['message', 'user', 'guild', 'channel'];
-    if (!types.includes(args[0])) { message.channel.send('ei sitä komentoa noin käytetä'); return; }
-
-    const ch = message.channel.id;
-
-    getSnowflake(args, client, ch)
-      .then(snowflake => getPastebin(snowflake))
-      .then((link) => {
-        if (link) message.channel.send(link);
-        else logger.error('Pastebin link was empty');
-      });
-  },
-};
-
-async function getSnowflake(args, client, ch) {
-  let obj;
-  switch (args[0]) {
-    case 'message': obj = getMessage(args[1], client, ch); break;
-    case 'user': obj = getUser(args[1], client, ch); break;
-    case 'guild': obj = getGuild(args[1], client, ch); break;
-    case 'channel': obj = getChannel(args[1], client, ch); break;
-  }
-  return await obj;
-}
 
 /**
  * Posts object to pastebin and returns the pastebin link
@@ -56,9 +27,10 @@ async function getPastebin(snowflake) {
   };
   const pastebin = axios.post(api, querystring.stringify(parameters))
     .then(response => response.data)
-    .then(data => data);
+    .then(data => data)
+    .catch((err) => { throw err; });
 
-  return await pastebin;
+  return pastebin;
 }
 
 /**
@@ -71,8 +43,8 @@ async function getPastebin(snowflake) {
 async function getMessage(id, client, ch) {
   const res = client.channels.get(ch).fetchMessage(id)
     .then(message => message)
-    .catch((err) => { logger.error(err); return 'Ei löytynyt viestiä'; });
-  return await res;
+    .catch((err) => { logger.error(err); throw new Error(`Message ${id} was not found`); });
+  return res;
 }
 
 /**
@@ -83,7 +55,7 @@ async function getMessage(id, client, ch) {
  */
 function getUser(id, client) {
   const user = client.users.get(id);
-  if (!user) return 'Ei löytynyt käyttäjää';
+  if (!user) throw new Error(`User ${id} was not found`);
   return user;
 }
 
@@ -95,7 +67,7 @@ function getUser(id, client) {
  */
 function getGuild(id, client) {
   const guild = client.guilds.get(id);
-  if (!guild) return 'Ei löytynyt kiltaa';
+  if (!guild) throw new Error(`Guild ${id} was not found`);
   return guild;
 }
 
@@ -107,6 +79,47 @@ function getGuild(id, client) {
  */
 function getChannel(id, client) {
   const channel = client.channels.get(id);
-  if (!channel) return 'Ei löytynyt kanavaa';
+  if (!channel) throw new Error(`Channel ${id} was not found`);
   return channel;
 }
+
+/**
+ * Handles discord object retrieval
+ * @param {Array} args - array of given arguments
+ * @param {Object} client - discord client
+ * @param {string} ch - channel the message was received on
+ * @returns {Object} discord object matching the arguments
+ */
+async function getSnowflake(args, client, ch) {
+  let obj;
+  switch (args[0]) {
+    case 'message': obj = await getMessage(args[1], client, ch); break;
+    case 'user': obj = getUser(args[1], client, ch); break;
+    case 'guild': obj = getGuild(args[1], client, ch); break;
+    case 'channel': obj = getChannel(args[1], client, ch); break;
+    default: throw new Error(`${args[0]} is not a valid type`);
+  }
+  return obj;
+}
+
+module.exports = {
+  name: 'print_test',
+  description: 'Posts a pastebin link containing the discord.js object (message/user/guild/channel) of the given type for the given id.',
+  usage: '[type] [id]',
+  execute(message, args, client) {
+    const types = ['message', 'user', 'guild', 'channel'];
+    if (!types.includes(args[0])) { message.channel.send('ei sitä komentoa noin käytetä'); return; }
+
+    const ch = message.channel.id;
+
+    getSnowflake(args, client, ch)
+      .then(snowflake => getPastebin(snowflake))
+      .then((link) => {
+        if (link) message.channel.send(link);
+        else throw new Error('Pastebin link was empty');
+      })
+      .catch((err) => {
+        message.channel.send(`\`\`\`${err.message}\`\`\``); logger.warn('%o', err.message);
+      });
+  },
+};
